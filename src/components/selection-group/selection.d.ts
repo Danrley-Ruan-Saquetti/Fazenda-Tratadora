de562ab177c54543586d95e98eb1fecfe8c3f5af
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-05-04 10:30:08 -0300

    Update selection.d.ts

--- src/components/selection-group/selection.d.ts
+++ src/components/selection-group/selection.d.ts
@@ -1,8 +1,9 @@
-type TOptionSelectionForm = "_newOne" | "_newAll" | "_clear"
-type TOptionSelectionFormAction = "_newOne" | "_newAll" | "_clear"
+type TOptionSelectionForm = '_newOne' | '_newAll' | '_clear';
+type TOptionSelectionFormAction = '_newOne' | '_newAll' | '_clear';
 type TOptionSelection = {
-    content: string,
-    type: string,
-    action: string,
-    submenu?: any[]
-}
+  content: string;
+  type: string;
+  action: string;
+  submenu?: any[];
+};
+type TFormResult = { value: any; type: string };
